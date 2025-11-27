ef71fa6523218dac62bc4143f08cb3d132e3fd07
Raj377 <101715179+Raj377@users.noreply.github.com>
2025-11-28 04:10:31 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     }
   });
 
-  const link = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + API_KEY;
+  const link = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=' + API_KEY;
   const googleUrl = url.parse(link);
 
   const options = {
